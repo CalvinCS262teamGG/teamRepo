b5c2f05e4f47271e8c6c95b4d36bc076fa480efd
Peter Haagsma <pjh26@maroon29.cs.calvin.edu>
2018-10-05 10:20:58 -0400

    Updated prototype

--- Hello Team GG!/app/src/main/java/edu/calvin/cs262/pjh26/helloteamgg/MainActivity.java
+++ Hello Team GG!/app/src/main/java/edu/calvin/cs262/pjh26/helloteamgg/MainActivity.java
@@ -1,9 +1,11 @@
 package edu.calvin.cs262.pjh26.helloteamgg;
 
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +23,23 @@ public class MainActivity extends AppCompatActivity {
     public void launchScheduleActivity(View view) {
         Intent intent = new Intent(this, Schedule.class);
         startActivity(intent);
+    }
+
+
+    public void toggleSelectedActivity(View view) {
+        Button toggleStart = (Button) findViewById(R.id.toggleStart);
+        Button toggleStop = (Button) findViewById(R.id.toggleStop);
+        if (toggleStart.getVisibility() == View.VISIBLE) {
+            toggleStart.setVisibility(View.INVISIBLE);
+            toggleStop.setVisibility(View.VISIBLE);
+        } else {
+            toggleStart.setVisibility(View.VISIBLE);
+            toggleStop.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 }
