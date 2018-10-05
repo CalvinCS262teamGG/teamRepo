5be6f562666398b6562f97e45d5d5ebd24338a32
Peter Haagsma <pjh26@maroon29.cs.calvin.edu>
2018-10-05 09:11:09 -0400

    Added prototype analytics page

--- Hello Team GG!/app/src/main/java/edu/calvin/cs262/pjh26/helloteamgg/MainActivity.java
+++ Hello Team GG!/app/src/main/java/edu/calvin/cs262/pjh26/helloteamgg/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.calvin.cs262.pjh26.helloteamgg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchAnalyticsActivity(View view) {
+        Intent intent = new Intent(this, Analytics.class);
+            startActivity(intent);
+    }
+
+    public void launchScheduleActivity(View view) {
+        Intent intent = new Intent(this, Schedule.class);
+        startActivity(intent);
     }
 }
